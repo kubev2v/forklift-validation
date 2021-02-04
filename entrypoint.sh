0f675a4f057df78e0b549791cf4b42c3e7d0a7e5
Peter McGowan <pmcgowan@redhat.com>
2021-02-04 14:12:02 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,16 @@
 set -e
 
 CMD="/usr/bin/opa run --server"
-if [ -f "$TLS_CERT_FILE" ] && [ -f "$TLS_KEY_FILE" ]; then
-  CMD="$CMD --tls-cert-file $TLS_CERT_FILE --tls-private-key-file $TLS_KEY_FILE"
-fi
-if [ -f "$TLS_CA_CERT_FILE" ]; then
-  CMD="$CMD --tls-ca-cert-file $TLS_CA_CERT_FILE"
+if [ "$TLS_ENABLED" == "true" ]; then
+  if [ -f "$TLS_CERT_FILE" ] && [ -f "$TLS_KEY_FILE" ]; then
+    CMD="$CMD --tls-cert-file $TLS_CERT_FILE --tls-private-key-file $TLS_KEY_FILE"
+  else
+    echo "ERROR: TLS_CERT_FILE and/or TLS_KEY_FILE variables not defined correctly"
+    exit 1 # terminate and indicate error
+  fi
+  if [ -f "$TLS_CA_CERT_FILE" ]; then
+    CMD="$CMD --tls-ca-cert-file $TLS_CA_CERT_FILE"
+  fi
 fi
 CMD="$CMD /usr/share/opa"
 
